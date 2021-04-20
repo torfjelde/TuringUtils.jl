87773bb9380be70a5931dd8b4dbfefd8a2a2e4a5
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-04-20 17:52:23 +0200

    added some more impls of optimize_bijector

--- src/packages/bijectors.jl
+++ src/packages/bijectors.jl
@@ -89,15 +89,35 @@ end
 #####################################################
 iscontiguous(left, right) = maximum(left) + 1 == minimum(right)
 
+Bijectors.up1(b::Bijectors.Stacked) = b
+
 optimize_bijector(b) = b
+optimize_bijector(ib::Bijectors.Inverse) = inv(optimize_bijector(ib.orig))
+
 function optimize_bijector(b::Bijectors.NamedBijector{names}) where {names}
     bs = map(b.bs) do b
         optimize_bijector(b)
     end
     return Bijectors.NamedBijector(NamedTuple{names}(bs))
 end
+
+# TODO: Maybe also handle the same cases as the evaluation of `TruncatedBijector`,
+# e.g. `Bijectors.Log{N}() ∘ Bijectors.Scale{N}(-lb)` if `all(isfinite.(lb))` i.e.
+# is lowerbounded.
+function optimize_bijector(b::Bijectors.TruncatedBijector{N}) where {N}
+    lb, ub = b.lb, b.ub
+    return if all(iszero.(lb)) && all(isinf.(ub))
+        Bijectors.Log{N}()
+    elseif all(isfinite.(lb)) && all(isfinite.(ub))
+        Bijectors.Logit{N}(lb, ub)
+    else
+        b
+    end
+end
+
 function optimize_bijector(b::Bijectors.Stacked)
     n = length(b.ranges)
+
     segments = UnitRange[]
     start_idx = 1
     i = start_idx
@@ -118,15 +138,21 @@ function optimize_bijector(b::Bijectors.Stacked)
     bs = map(segments) do r
         b_r = b.bs[minimum(r)]
         if length(r) > 1 && Bijectors.dimension(b_r) == 0
-            Bijectors.up1(b_r)
+            optimize_bijector(Bijectors.up1(b_r))
         else
-            b_r
+            optimize_bijector(b_r)
         end
     end
 
     ranges = map(segments) do r
         minimum(b.ranges[minimum(r)]):maximum(b.ranges[maximum(r)])
     end
+
+    @assert length(bs) == length(ranges) "number of bijectors and ranges is different"
+
+    # Nvm; this actually causes issues for certain bijectors which we can't do `up1` to.
+    # # If we're left with only a single bijector, don't wrap in `Stacked`.
+    # length(bs) == 1 && return optimize_bijector(b.bs[1])
 
     return Bijectors.Stacked(bs, ranges)
 end
