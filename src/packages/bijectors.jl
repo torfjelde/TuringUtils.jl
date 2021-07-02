99912f62980f900c8d3f8386293ec36cd8982069
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-07-02 01:01:36 +0100

    removed now redundant Stacked2

--- src/packages/bijectors.jl
+++ src/packages/bijectors.jl
@@ -31,58 +31,6 @@ E.g. `ComponentArrays.ComponentArray(varinfo)`, `namedtuple(varinfo)`.
     end
 end
 
-# Relaxed `Bijectors.Stacked`
-# TODO: Just relax the constraints on `Stacked` in the Bijectors.jl.
-struct Stacked2{Bs, Rs} <: Bijectors.Bijector{1}
-    bs::Bs
-    ranges::Rs
-end
-
-Base.inv(sb::Stacked2) = Stacked2(map(Base.inv, sb.bs), sb.ranges)
-
-function (sb::Stacked2{<:AbstractArray})(x::AbstractVector{<:Real})
-    n = length(sb.ranges)
-    n == 1 && return sb.bs[1](x[sb.ranges[1]])
-
-    y = mapreduce(vcat, 1:n) do i
-        sb.bs[i](x[sb.ranges[i]])
-    end
-    @assert size(y) == size(x) "x is size $(size(x)) but y is $(size(y))"
-    return y
-end
-
-function Bijectors.logabsdetjac(
-    b::Stacked2{<:AbstractArray},
-    x::AbstractVector{<:Real}
-)
-    n = length(b.ranges)
-    init = sum(Bijectors.logabsdetjac(b.bs[1], x[b.ranges[1]]))
-    return mapreduce(+, 2:n; init = init) do i
-        sum(Bijectors.logabsdetjac(b.bs[i], x[b.ranges[i]]))
-    end
-end
-
-function Bijectors.logabsdetjac(b::Stacked2, x::AbstractMatrix{<:Real})
-    return map(eachcol(x)) do c
-        Bijectors.logabsdetjac(b, c)
-    end
-end
-
-function Bijectors.forward(sb::Stacked2{<:AbstractArray}, x::AbstractVector)
-    n = length(sb.ranges)
-    yinit, linit = Bijectors.forward(sb.bs[1], x[sb.ranges[1]])
-
-    n == 1 && return (rv = yinit, logabsdetjac = sum(linit))
-    
-    logjac = sum(linit)
-    ys = Bijectors.mapvcat(drop(sb.bs, 1), drop(sb.ranges, 1)) do b, r
-        y, l = forward(b, x[r])
-        logjac += sum(l)
-        y
-    end
-    return (rv = vcat(yinit, ys), logabsdetjac = logjac)
-end
-
 
 #####################################################
 ### Try to optimize the structure of the bijector ###
