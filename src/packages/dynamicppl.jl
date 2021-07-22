6962bd47f789f943f03adbcba7ccb3e3f7467dd6
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2021-07-22 05:32:03 +0100

    more utils for DynamicPPL

--- src/packages/dynamicppl.jl
+++ src/packages/dynamicppl.jl
@@ -1,4 +1,9 @@
+module DynamicPPLUtils
+
 using DynamicPPL
+
+using Random: Random
+using ProgressMeter: ProgressMeter
 
 # Convenient overload
 function DynamicPPL.set_flag!!(vi, vns::AbstractVector{<:VarName}, flag)
@@ -10,6 +15,16 @@ function DynamicPPL.set_flag!!(vi, vns::AbstractVector{<:VarName}, flag)
     return new_vi
 end
 
+############################
+# Simple utility functions #
+############################
+function replace_args(m::Model; kwargs...)
+    return Model(m.name, m.f, deepcopy(merge(m.args, kwargs)), deepcopy(m.defaults))
+end
+
+#############################################
+# Fast `setval!` and `setval_and_resample!` #
+#############################################
 """
     fast_setval!!(vi; x...)
     fast_setval!!(vi, x::NamedTuple)
@@ -77,7 +92,7 @@ repeatedly on the metadata for each of the variables.
 """
 fast_setval_and_resample!!(vi::TypedVarInfo; kwargs...) = fast_setval_and_resample!!(vi, (;kwargs...))
 fast_setval_and_resample!!(vi::TypedVarInfo, x::NamedTuple) = fast_setval_and_resample!!(vi, vi.metadata, x)
-@generated function fast_setval_and_resample!(
+@generated function fast_setval_and_resample!!(
     vi::TypedVarInfo,
     metadata::NamedTuple{destnames},
     x::NamedTuple{srcnames},
@@ -121,7 +136,6 @@ function fast_setval_and_resample_inner!!(vi::TypedVarInfo, md, val)
 
     return vi
 end
-
 
 """
     hasindex(val, vn::VarName)
@@ -205,4 +219,111 @@ hasindex(val, index::Tuple{<:AbstractVector{Int}}) = all(first(index) .≤ lengt
 function hasindex(val, index::Tuple{<:Any}, indexing...)
     i = first(index)
     return hasindex(val, index) && hasindex(view(val, CartesianIndices(val)[i]), indexing...)
+end
+
+#######################################
+# Prediction and generated quantities #
+#######################################
+"""
+    fast_generated_quantities(model, chain)
+
+Fast version of [`DynamicPPL.generated_quantities`](@ref) using `NamedTupleChainIterator`
+together with [`fast_setval_and_resample!!`](@ref) to achieve high performance.
+"""
+function fast_generated_quantities(model::DynamicPPL.Model, chain::DynamicPPL.AbstractChains)
+    varinfo = DynamicPPL.VarInfo(model)
+    iters = NamedTupleChainIterator(keys(varinfo.metadata), chain, getconverters(chain))
+
+    pm = ProgressMeter.Progress(length(iters))
+
+    results = map(iters) do nt
+        TuringUtils.fast_setval_and_resample!!(varinfo, nt)
+        result = model(varinfo)
+
+        ProgressMeter.next!(pm)
+
+        return result
+    end
+
+    return reshape(results, size(chain, 1), size(chain, 3))
+end
+
+"""
+    fast_predict(model, chain; kwargs...)
+
+Fast version of [`Turing.Inference.predict`](@ref) using `NamedTupleChainIterator`
+together with [`fast_setval_and_resample!!`](@ref) to achieve high performance.
+"""
+function fast_predict(model::Model, chain::MCMCChains.Chains; kwargs...)
+    return fast_predict(Random.GLOBAL_RNG, model, chain; kwargs...)
+end
+function fast_predict(
+    rng::Random.AbstractRNG, model::Model, chain::MCMCChains.Chains;
+    include_all = false
+)
+    # Don't need all the diagnostics
+    chain_parameters = MCMCChains.get_sections(chain, :parameters)
+
+    spl = DynamicPPL.SampleFromPrior()
+
+    # Sample transitions using `spl` conditioned on values in `chain`
+    transitions = fast_transitions_from_chain(rng, model, chain_parameters; sampler = spl)
+
+    # Let the Turing internals handle everything else for you
+    chain_result = reduce(
+        MCMCChains.chainscat, [
+            AbstractMCMC.bundle_samples(
+                transitions[:, chain_idx],
+                model,
+                spl,
+                nothing,
+                MCMCChains.Chains
+            ) for chain_idx = 1:size(transitions, 2)
+        ]
+    )
+
+    parameter_names = if include_all
+        names(chain_result, :parameters)
+    else
+        filter(k -> ∉(k, names(chain_parameters, :parameters)), names(chain_result, :parameters))
+    end
+
+    return chain_result[parameter_names]
+end
+
+function fast_transitions_from_chain(
+    model::Turing.Model,
+    chain::MCMCChains.Chains;
+    sampler = DynamicPPL.SampleFromPrior()
+)
+    return fast_transitions_from_chain(Random.GLOBAL_RNG, model, chain; sampler)
+end
+function fast_transitions_from_chain(
+    rng::Random.AbstractRNG,
+    model::Turing.Model,
+    chain::MCMCChains.Chains;
+    sampler = DynamicPPL.SampleFromPrior()
+)
+    varinfo = Turing.VarInfo(model)
+    iters = NamedTupleChainIterator(keys(varinfo.metadata), chain, getconverters(converters))
+
+    pm = ProgressMeter.Progress(length(iters))
+
+    transitions = map(iters) do nt
+        # Set variables present in `chain` and mark those NOT present in chain to be resampled.
+        TuringUtils.fast_setval_and_resample!!(varinfo, nt)
+        model(rng, varinfo, sampler)
+
+        # Convert `VarInfo` into `NamedTuple` and save.
+        theta = DynamicPPL.tonamedtuple(varinfo)
+        lp = Turing.getlogp(varinfo)
+
+        ProgressMeter.next!(pm)
+
+        Turing.Inference.Transition(theta, lp)
+    end
+
+    return reshape(transitions, size(chain, 1), size(chain, 3))
+end
+
 end
